68acff33bd90a60176dfa7d89a3dc57552121657
SaKaSe2 <gmrikza05@gmail.com>
2025-10-30 11:33:23 +0700

    Update_Animasi

--- lib/controllers/menu_controller.dart
+++ lib/controllers/menu_controller.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../models/menu_item.dart';
-import '../services/api_service.dart'; // file saklar untuk http/dio
+import '../services/api_service.dart';
 
 class MenuController extends GetxController {
   var menuItems = <MenuItem>[].obs;
@@ -32,14 +32,67 @@ class MenuController extends GetxController {
 
   void addToCart(MenuItem item) {
     cartItems.add(item);
+    
+    // Snackbar simple & elegan
     Get.snackbar(
-      'Keranjang',
-      '${item.name} ditambahkan ke keranjang',
-      snackPosition: SnackPosition.BOTTOM,
+      '',
+      '',
+      titleText: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: const Color(0xFFD4A017).withOpacity(0.2),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const Icon(
+              Icons.check_circle_outline,
+              color: Color(0xFFD4A017),
+              size: 22,
+            ),
+          ),
+          const SizedBox(width: 12),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Ditambahkan ke Keranjang',
+                  style: TextStyle(
+                    color: Color(0xFFD4A017),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 13,
+                  ),
+                ),
+                const SizedBox(height: 2),
+                Text(
+                  item.name,
+                  style: const TextStyle(
+                    color: Colors.white70,
+                    fontSize: 12,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      messageText: const SizedBox.shrink(),
+      snackPosition: SnackPosition.TOP,
       backgroundColor: const Color(0xFF2D2D2D),
-      colorText: const Color(0xFFD4A017),
-      margin: const EdgeInsets.all(10),
-      animationDuration: const Duration(milliseconds: 400),
+      borderRadius: 12,
+      margin: const EdgeInsets.only(top: 10, left: 12, right: 12),
+      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+      duration: const Duration(milliseconds: 1800),
+      animationDuration: const Duration(milliseconds: 300),
+      borderColor: const Color(0xFFD4A017).withOpacity(0.3),
+      borderWidth: 1,
+      isDismissible: true,
+      dismissDirection: DismissDirection.up,
+      forwardAnimationCurve: Curves.easeOut,
+      reverseAnimationCurve: Curves.easeIn,
     );
   }
 
